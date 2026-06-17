ab4c956a792e0818910ff55068df95fb33dd6390
Michael Bischoff <Michael.Bischoff@bertelsmann.de>
2026-06-17 12:32:10 +0200

    BON-34: delete some outdated projects

--- bonaparte-hazelcast/src/main/java/de/jpaw/bonaparte/hazelcast/BonapartePortable.java
+++
@@ -1,9 +0,0 @@
-package de.jpaw.bonaparte.hazelcast;
-
-import com.hazelcast.nio.serialization.Portable;
-
-import de.jpaw.bonaparte.core.BonaPortable;
-
-/** Common interface for classes implementing both Portable and BonaPortable. */
-public interface BonapartePortable extends BonaPortable, Portable {
-}
