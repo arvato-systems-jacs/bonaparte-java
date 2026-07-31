943a39fb7b998ad23a00befbc0ba9e073c0c5898
Michael Bischoff <Michael.Bischoff@bertelsmann.de>
2026-07-31 10:45:24 +0200

    fix xtend => Java migration

--- persistence-api/src/main/java/de/jpaw/bonaparte/jpa/api/JpaCriteriaBuilder.java
+++ persistence-api/src/main/java/de/jpaw/bonaparte/jpa/api/JpaCriteriaBuilder.java
@@ -1,29 +1,32 @@
 package de.jpaw.bonaparte.jpa.api;
 
+import jakarta.annotation.Nonnull;
+import jakarta.annotation.Nullable;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.Predicate;
+
 import de.jpaw.bonaparte.pojos.api.AndFilter;
 import de.jpaw.bonaparte.pojos.api.FieldFilter;
 import de.jpaw.bonaparte.pojos.api.NotFilter;
 import de.jpaw.bonaparte.pojos.api.OrFilter;
 import de.jpaw.bonaparte.pojos.api.SearchFilter;
-import de.jpaw.dp.Inject;
-import jakarta.persistence.criteria.CriteriaBuilder;
-import jakarta.persistence.criteria.Predicate;
+import de.jpaw.dp.Jdp;
 
 public class JpaCriteriaBuilder {
-    @Inject
-    private JpaFilter jpaFilter;
+    private final JpaFilter jpaFilter = Jdp.getRequired(JpaFilter.class);
 
     private final JpaPathResolver pathResolver;
     private final CriteriaBuilder cb;
 
-    public JpaCriteriaBuilder(JpaPathResolver pathResolver, CriteriaBuilder cb) {
+    public JpaCriteriaBuilder(@Nonnull final JpaPathResolver pathResolver, @Nonnull final CriteriaBuilder cb) {
         this.pathResolver = pathResolver;
         this.cb = cb;
     }
 
-    public Predicate buildPredicate(SearchFilter filter) {
-        if (filter == null)
+    public Predicate buildPredicate(@Nullable final SearchFilter filter) {
+        if (filter == null) {
             return null;
+        }
         return switch (filter) {
             case FieldFilter f -> jpaFilter.applyFilter(cb, pathResolver.getPath(f.getFieldName()), f);
             case AndFilter f -> cb.and(buildPredicate(f.getFilter1()), buildPredicate(f.getFilter2()));
